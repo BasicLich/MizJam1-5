bd031868d55d4e1ce8bacecb8b7929c0ee16f32d
Tourmi <tourmithefly@hotmail.com>
2020-08-17 14:40:28 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MizJam1
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
